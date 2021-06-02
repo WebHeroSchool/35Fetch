aa09b44ce8d4e3f91f02868b1b9cb110c5342b3e
Sergey_N83 <Sergey_n83@inbox.ru>
2021-06-02 14:25:08 +0300

    обновление

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let getUserName = (url) => {
     userName = "SergeyN83";
   }
     return userName;
-}
+};
 
 let userName = getUserName(url);
 console.log(userName);
@@ -20,10 +20,43 @@ fetch(`https://api.github.com/users/${userName}`)
         let avatar = json.avatar_url;
         let name = json.name;
         let bio = json.bio;
-        let url = `https://github.com/${name}`;
+        let url = json.url;
         console.log(avatar);
         console.log(name);
         console.log(bio);
         console.log(url);
+
+
+        let modifyName = () => {
+            let userName = document.createElement("h1") ;
+            userName.innerHTML = name;
+            document.body.append(userName);
+          }
+      
+          let modifyBio = () => {
+            let userBio = document.createElement("p");
+            userBio.innerHTML = bio;
+            document.body.appendChild(userBio);
+            
+          }
+          let modifyAvatar = () => {
+            let userAvatar = document.createElement("img");
+            userAvatar.src = avatar;
+            userAvatar.alt = "Avatar";
+            document.body.appendChild(userAvatar);
+          }
+          let modifyUrl = () => {
+            let userUrl = document.createElement("a");
+            let text = document.createTextNode('Ссылка на страницу GitHub');
+            let newString = document.createElement('br');
+            userUrl.href = `https://github.com/${userName}`;
+            userUrl.appendChild(text);
+            document.body.appendChild(newString);
+            document.body.appendChild(userUrl);
+          }
+          modifyName();
+          modifyBio();
+          modifyAvatar();
+          modifyUrl();
     })
-    .catch(err => document.write("Информация о пользователе не доступна"));
+    .catch(err => alert(err + 'Информация о пользователе не доступна'));
