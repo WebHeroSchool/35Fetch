a29c2d8f425f3624a14fea476f4565ded77173f7
Sergey_N83 <Sergey_n83@inbox.ru>
2021-06-02 13:25:54 +0300

    получаю данные из api

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-fetch('https://api.github.com/users/SergeyN83')
+let url = window.location.toString();
+
+let getUserName = (url) => {
+  let getName = url.split("=");
+  let userName = getName[1];  
+  if (userName == undefined){
+    userName = "SergeyN83";
+  }
+    return userName;
+}
+
+let userName = getUserName(url);
+console.log(userName);
+
+
+fetch(`https://api.github.com/users/${userName}`)
     .then(res => res.json())
-    .then(json => console.log(json))
-    .catch(err => console.log(err));
+    .then(json => {
+        // console.log(json)
+        let avatar = json.avatar_url;
+        let name = json.name;
+        let bio = json.bio;
+        let url = `https://github.com/${name}`;
+        console.log(avatar);
+        console.log(name);
+        console.log(bio);
+        console.log(url);
+    })
+    .catch(err => document.write("Информация о пользователе не доступна"));
